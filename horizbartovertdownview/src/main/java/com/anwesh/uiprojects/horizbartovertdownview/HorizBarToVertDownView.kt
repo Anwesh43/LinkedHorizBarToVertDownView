b57851da742f85b8f23157eb320c1a381ae6c645
Anwesh43 <anweshthecool0@gmail.com>
2019-02-09 09:59:32 +0530

    adding libraries and constants

--- horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
+++ horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.horizbartovertdownview
 /**
  * Created by anweshmishra on 09/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 10
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val barColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#212121")
+val delay : Long = 20
