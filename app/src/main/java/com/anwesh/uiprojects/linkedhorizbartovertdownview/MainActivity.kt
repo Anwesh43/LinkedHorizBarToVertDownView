23019f6b2bdd528e2e67ef16dc296bfba6aeec0f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-09 10:39:02 +0530

    adding fullscreenn support

--- app/src/main/java/com/anwesh/uiprojects/linkedhorizbartovertdownview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhorizbartovertdownview/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.anwesh.uiprojects.linkedhorizbartovertdownview
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.horizbartovertdownview.HorizBarToVertDownView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HorizBarToVertDownView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
