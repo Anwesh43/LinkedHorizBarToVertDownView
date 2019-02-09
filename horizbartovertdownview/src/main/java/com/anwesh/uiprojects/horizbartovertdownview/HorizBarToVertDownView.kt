e7a761d6d33ec49654b5f0615b7b611d756c4c8b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-09 10:14:10 +0530

    addng HorizBarToVertDownView class

--- horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
+++ horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
@@ -46,3 +46,21 @@ fun Canvas.drawHBTVDNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class HorizBarToVertDownView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
