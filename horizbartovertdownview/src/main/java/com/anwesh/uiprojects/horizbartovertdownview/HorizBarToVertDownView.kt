ed666b8b58c9cd6dcc1372aedee328dd8854a1b7
Anwesh43 <anweshthecool0@gmail.com>
2019-02-09 10:22:07 +0530

    adding HBTVDNode class

--- horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
+++ horizbartovertdownview/src/main/java/com/anwesh/uiprojects/horizbartovertdownview/HorizBarToVertDownView.kt
@@ -107,8 +107,51 @@ class HorizBarToVertDownView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class HBTVDNode(var i : Int, val state : State = State()) {
+
+        private var next : HBTVDNode? = null
+        private var prev : HBTVDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HBTVDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHBTVDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HBTVDNode {
+            var curr : HBTVDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
